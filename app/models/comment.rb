f4b700f6e0b2a2782d7d4cd05d0c132f09082dba
Felix Odete <felix.odette@gmail.com>
2022-08-26 07:43:17 +0300

    Update likes module

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post, counter_cache: true
   belongs_to :author, class_name: 'User', foreign_key: true
-  
+
   def update_comments_counter
     post.increment!(:comments_count)
   end
