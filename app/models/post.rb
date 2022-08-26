863a872ebe5ca7cf0ea6d574759a30635953a01e
Felix Odete <felix.odette@gmail.com>
2022-08-26 07:44:42 +0300

    Update posts module

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: true
   has_many :likes
   has_many :comments
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
