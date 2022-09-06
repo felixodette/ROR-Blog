82eb59dfb12fd49505cdbfdcd0b5c166fa4c3438
Felix Odete <felix.odette@gmail.com>
2022-09-06 14:42:54 +0300

    Update tests: user_show and comment_spec

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'user_show', type: :feature do
+  user = User.first
+
   before(:each) do
-    visit(user_path(User.first.id))
+    visit (user_path(User.first.id))
   end
 
   it 'shows the user\'s profile picture' do
@@ -10,17 +12,14 @@ RSpec.describe 'user_show', type: :feature do
   end
 
   it 'can see the user\'s username' do
-    user = User.first
     expect(page).to have_content(user.name)
   end
 
   it 'can see the user\'s number of posts' do
-    user = User.first
     expect(page).to have_content("Number of posts: #{user.posts_count}")
   end
 
   it 'can see the user\'s bio' do
-    user = User.first
     expect(page).to have_content(user.bio)
   end
 
@@ -33,15 +32,13 @@ RSpec.describe 'user_show', type: :feature do
   end
 
   it 'redirects to the post\'s page when a post is clicked' do
-    user = User.first
     post = user.posts.first
     click_link(post.title)
     expect(page.current_path).to eql(user_post_path(user_id: user.id, id: post.id))
   end
 
   it 'redirects to the user\'s posts page when the button See all posts is clicked' do
-    user = User.first
     click_link('See all posts')
-    expect(page.current_path).to eql(user_posts_path(user_id: user.id).to_s)
+    expect(page.current_path).to eql("#{user_posts_path(user_id: user.id)}")
   end
 end
